f17b3ab5c0adeeb27604dda50e1dd1abb4f0951d
mario8912 <lopezvilamario98@gmail.com>
2024-01-19 00:45:54 +0100

    gitignore anadido, proyecto creado v1

---
+++ Datos/DatosParametrosReporte.cs
@@ -0,0 +1,37 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.ReportAppServer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DatosParametrosReporte
+    {
+        private ReportDocument _reporte;
+        private string _rutaInformeTxt;
+        private StreamWriter _streamEscritura;
+
+        public DatosParametrosReporte(ReportDocument reporte, string rutaInforme)
+        {
+            _reporte = reporte;
+            _rutaInformeTxt = Path.ChangeExtension(rutaInforme, ".txt");
+            _streamEscritura = new StreamWriter(_rutaInformeTxt);
+        }
+        public void GeneraTxtParamentrosReporte()
+        {
+            foreach (ParameterFieldDefinition datosDelParametro in _reporte.DataDefinition.ParameterFields)
+            {
+                _streamEscritura.WriteLine(string.Format("{0}|{1}|{2}", 
+                    datosDelParametro.ParameterFieldName, 
+                    datosDelParametro.ParameterValueKind,
+                    datosDelParametro.DiscreteOrRangeKind));
+            }
+            _streamEscritura.Close();
+        }
+
+    }
+}
---
+++ Negocio/NegocioParametrosReporte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrystalDecisions.CrystalReports.Engine;
+using Datos;
+
+
+namespace Negocio
+{
+    public static class NegocioParametrosReporte
+    {
+        public static void GenerarTxtParamentros(ReportDocument reporte, string rutaReporte)
+        { 
+            DatosParametrosReporte parametrosReporte = new DatosParametrosReporte(reporte, rutaReporte);
+            parametrosReporte.GeneraTxtParamentrosReporte();
+        }
+    }
+}
